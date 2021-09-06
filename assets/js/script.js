88799fc29929c295b8f9d15353dee49996afa492
osbym <marosb79@yahoo.com>
2021-09-05 23:15:38 -0500

    timer added

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,8 +64,62 @@ var questions = [
                 "Ampersand, colon",
                     "Ampersand, semicolon"],
                     "Ampersand, semicolon"
-    
-    
-    ]
-    )
+    ),
 ]
+
+// Timer countdown information
+function startTimer() {
+    var p_time = document.getElementById('time').innerHTML;
+    var timeArray = p_time.split(":");
+    var min = timeArray[0];
+    var sec = checkSecond((timeArray[1] - 1));
+    if (sec == 59) {
+        min = min -1
+    }
+
+    if (min < 0) {
+        clearTimeout(timerRef)
+        alert("Out of Time")
+        return;
+    }
+
+    document.getElementById('time').innerHTML = min + ":" + sec;
+
+    var timeRef = setTimeout(startTimer, 2000);
+
+}
+
+function checkSecond(seconds) {
+    if (seconds < 10 && seconds >= 0) {
+        seconds = "0" + seconds
+    };
+    if (seconds < 0) {
+        seconds = "59"
+    };
+    return seconds;
+
+    }
+    
+function wrongTimer () {
+    var x_time = document.getElementById('time').innerHTML;
+    var wrongArray = x_time.split(":");
+    var x_min = wrongArray[0];
+    var x_sec = checkSecond((wrongArray[1]-1));
+    if(x_sec == 59) {
+        x_min = x_min - 1
+
+    }
+
+    x_sec -= 10;
+    document.getElementById('time').innerHTML = x_min + ":" + x_sec;
+}
+
+function stopTimer () {
+    clearInterval(timerRef);
+}
+
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex =0;
+}

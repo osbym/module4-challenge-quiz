47f5beb1248a96512a07466925fabc45db90981f
osbym <marosb79@yahoo.com>
2021-09-05 23:42:40 -0500

    added event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -123,3 +123,81 @@ function Quiz(questions) {
     this.questions = questions;
     this.questionIndex =0;
 }
+
+Quiz.prototype.getQuestionIndex = function () {
+    return this.questions[this.questionIndex];
+
+}
+
+Quiz.prototype.guess = function (answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        right.innerHTML = "Correct!";
+        this.score += 10;
+    } else if (!this.getQuestionIndex().isCorrectAnswer(answer)) {
+        right.innerHTML = "Incorrect - Minus 10 Seconds";
+        wrongTimer();
+    }
+    this.questionIndex++;
+
+}
+
+Quiz.prototype.isEnded = function () {
+    return this.questionIndex === this.questions.length;
+}
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        var quesEl = document.getElementById("question");
+        quesEl.innerHTML = quiz.getQuestionIndex().text;
+
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i =0; i < choices.length; i++) {
+            var quesEl = document.getElementById("choice"+i);
+            questionEl.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+      }
+    };
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function () {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+function showScores() {
+    var initials = window.prompt("Please enter your initals to record your score.")
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + initials + " " + quiz.score + "</h2>";
+    localStorage.setItem("highsores", initials + " " + quiz.score);
+    redo.textContent = "Replay";
+    startButton.style.display = "none";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+//Start Quiz
+var quiz = new Quiz(questions);
+
+function replay () {
+    location.reload();
+};
+
+startButton.addEventListener('click', startTimer);
+startButton.addEventListener('click', populate);
+redo.addEventListener('click', replay);

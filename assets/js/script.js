98a3d7eb17222537ba6acfadc69c2a7367572ae4
osbym <marosb79@yahoo.com>
2021-09-06 00:32:01 -0500

    debugged the timer element

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@ const startButton = document.getElementById('start');
 var right = document.getElementById("response");
 var redo = document.getElementById("replay");
 
+
+
 // Coding Challenge Questions
 var questions = [
     new Question(
-        "1. What are variables used for in JavaScript Programs?",
+        "What are variables used for in JavaScript Programs?",
         ["Storing numbers, dates, or other values", 
         "Varying randomly", 
         "Causing high-school flashbacks", 
@@ -14,7 +16,7 @@ var questions = [
     ),
 
     new Question(
-        "2. Which of the following are capabilities of functions in JavaScript?",
+        "Which of the following are capabilities of functions in JavaScript?",
         ["Return a value", 
         "Accept parameters and Return a value",
         "Accept parameters", 
@@ -23,15 +25,15 @@ var questions = [
     ),
 
     new Question(
-        "3. Which tag is an extention to HTML that can enclose any number of JavaScript statements?",
-        ["<SCRIPT>",
-        "<BODY>", "<HEAD>",
-        "<TITLE>"],
-        "<SCRIPT>"
+        "Which tag is an extention to HTML that encloses any number of statements?",
+        ["SCRIPT",
+        "BODY", "HEAD",
+        "TITLE"],
+        "SCRIPT"
     ),
 
     new Question(
-        "4. Which of the following is not considered a JavaScript operator?",
+        "Which of the following is not considered a JavaScript operator?",
         ["new",
             "this",
                 "delete",
@@ -40,16 +42,16 @@ var questions = [
     ),
 
     new Question(
-        "5. Which of the following best describes JavaScript?",
+        "Which of the following best describes JavaScript?",
         ["a low-level programming language.",
-            "a scripting language precompiled in the browser.",
+            "a language precompiled in the browser.",
             "a compiled scripting language",
             "an object-oriented scripting language"],
             "an object-oriented scripting language"
     ),
 
     new Question(
-        "6. Which statement tests for a specific condition",
+        "Which statement tests for a specific condition?",
         ["Select",
             "If",
                 "Switch",
@@ -58,7 +60,7 @@ var questions = [
     ),
     
     new Question(
-        "7. Which answer best describe what JavaScript entities start and and end with?",
+        "Which answer describes what JavaScript entities start and and end with?",
         ["Semicolon, colon",
             "Semicolon, Ampersand",
                 "Ampersand, colon",
@@ -85,7 +87,7 @@ function startTimer() {
 
     document.getElementById('time').innerHTML = min + ":" + sec;
 
-    var timeRef = setTimeout(startTimer, 2000);
+    var timeRef = setTimeout(startTimer, 1000);
 
 }
 
@@ -191,7 +193,7 @@ function showScores() {
     element.innerHTML = gameOverHTML;
 };
 
-//Start Quiz
+//Start quiz when "start" button is pushed
 var quiz = new Quiz(questions);
 
 function replay () {

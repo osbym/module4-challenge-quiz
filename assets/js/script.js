0ea768d2b0612134f216918b74a4ac7f7955a496
osbym <marosb79@yahoo.com>
2021-09-05 22:39:21 -0500

    added intial questions

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+const startButton = document.getElementById('start');
+var right = document.getElementById("response");
+var redo = document.getElementById("replay");
+
+// Coding Challenge Questions
+var questions = [
+    new Question(
+        "1. What are variables used for in JavaScript Programs?",
+        ["Storing numbers, dates, or other values", 
+        "Varying randomly", 
+        "Causing high-school flashbacks", 
+        "None of the above"],
+        "Storing numbers, dates, or other values"
+    ),
+
+    new Question(
+        "2. Which of the following are capabilities of functions in JavaScript?",
+        ["Return a value", 
+        "Accept parameters and Return a value",
+        "Accept parameters", 
+        "None of the above"],
+        "Accept parameters"
+    ),
+
+    new Question(
+        
+    )
+]
